3f59b896fde6ac535abcbb7e570c29c94b854426
iSmash <nemeth.richo@gmail.com>
2018-08-19 13:54:46 +0100

    implemented validations

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,3 @@
 class Portfolio < ApplicationRecord
+	validates_presence_of :title, :body #Ez a record crealasnal a kitel, mi nem lehet nill 
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,3 @@
 class Skill < ApplicationRecord
+	validates_presence_of :title, :percente_utilized #Ez a record crealasnal a kitel, mi nem lehet nill
 end
