a6fbc52c2822fb63882f97736f278dc772c21e45
iSmash <nemeth.richo@gmail.com>
2018-08-22 15:08:07 +0100

    managing data

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,5 @@
 class Portfolio < ApplicationRecord
+	include Placeholder
 	validates_presence_of :title, :body #Ez a record crealasnal a kitetel, mi nem lehet nill 
     
   def self.angular #self azt jelenti hogy az aktulais portfolio itemket veszil alapul, es ezzle csinalunk egy caustom scoopt ahoz hogyt szurhessunk cim alapjan
@@ -8,5 +9,12 @@ class Portfolio < ApplicationRecord
   def self.rails
   	where(subtitle: 'Rails')
   end
+
+  after_initialize :set_defaults
+
+    def set_defaults
+    	self.main_image ||= Placeholder.image_generator(height: '350', width: '650')
+    	self.thumb_image ||= Placeholder.image_generator(height: '250', width: '350')
+    end
 
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,10 @@
 class Skill < ApplicationRecord
+	include Placeholder
 	validates_presence_of :title, :percent_utilized #Ez a record crealasnal a kitel, mi nem lehet nill
+
+    after_initialize :set_defaults
+
+    def set_defaults
+    	self.badge ||= Placeholder.image_generator(height: '250', width: '350')
+    end
 end
