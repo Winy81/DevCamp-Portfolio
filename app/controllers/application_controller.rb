e4c61348dfed397e5f30e6e4a97a88ff88f37c06
iSmash <nemeth.richo@gmail.com>
2018-09-14 14:55:34 +0100

    Added nav bar and added default header content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,7 @@ class ApplicationController < ActionController::Base
 	include DeviseWhitelist
 	include SetSource #ez ahoz van hogy ha valahonna. mashonnan logolnak ide
 	include CurrentUserConcern #belogolt es guest userek megkulonboztetesere
+    include DefaultPageContent #Alap cimeballitas a bongeszo fuleben
+	
 
 end
---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,13 @@
+module DefaultPageContent
+	extend ActiveSupport::Concern
+
+	included do
+ 	before_action :set_page_defaults
+    end
+#dinamikus cim beallitas a bongeszo fulben...
+    def set_page_defaults
+		@page_title = "Devcamp Portfolio | My Portfolio website"
+		@seo_keywords = "Adam Nemeth portfolio"
+	end
+
+ end
